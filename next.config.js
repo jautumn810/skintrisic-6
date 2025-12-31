7ee3445a68ac384549c8ce835be23c54cd8723e8
Autumn <jautumn810@gmail.com>
2025-12-31 03:00:17 -0600

    Fix Next.js build: Convert config to ES module syntax and update build scripts

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // If you're using static HTML files, you might want to use 'export'
-  // output: 'export',
 }
 
-module.exports = nextConfig
+export default nextConfig
 
